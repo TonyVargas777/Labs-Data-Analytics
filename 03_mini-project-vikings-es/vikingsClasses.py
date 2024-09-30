2866405b8c3d1262cb12076eb2371d2fe7c36055
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2024-09-30 17:24:20 +0200

    update labs 30/09/24

---
+++ 03_mini-project-vikings-es/vikingsClasses.py
@@ -0,0 +1,116 @@
+import random
+
+# Soldier
+
+
+class Soldier:
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+    
+
+# Viking
+
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+
+    def attack(self):
+        return self.strength
+
+    def battleCry(self):
+        return 'Odin Owns You All!'
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            #return self.name + ' has received 70 points of damage'
+            #return self.name + ' has received 50 points of damage'
+            return f'{self.name} has received {damage} points of damage'
+        else:
+            #return self.name + ' has died in act of combat'
+            return f'{self.name} has died in act of combat'
+            #self.name + ' has died in act of combat'
+
+
+
+# Saxon
+
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            #return f"Un 'Saxon' ha recibido {damage} puntos de daño."
+            return f'A Saxon has received {damage} points of damage'
+        else:
+            return f'A Saxon has died in combat'
+
+
+
+
+# Davicente
+
+class War():
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, viking):
+        self.vikingsArmy.append(viking)
+    
+    def addSaxon(self, saxon):
+        self.saxonArmy.append(saxon)
+    
+    def vikingAttack(self):
+        saxon.health -= viking.strength
+        if saxon.health <= 0:
+            saxonArmy.pop()
+        return f'{receiveDamage()} {Saxon} {strength} {Viking}'
+    
+    def saxonAttack(self):
+        return f'{receiveDamage()} {Viking} {strength} {Saxon}'
+
+    def showStatus(self):
+        if len(saxonArmy) == 0:
+            return 'Vikings have won the war of the century!'
+        if len(vikingArmy) == 0:
+            return 'Saxons have fought for their lives and survive another day...'
+        else:
+            return 'Vikings and Saxons are still in the thick of battle.'
+
+
+'''
+#yop
+class War2:
+
+    def __init__(self):
+        # your code here
+
+    def addViking(self, Viking):
+        # your code here
+    
+    def addSaxon(self, Saxon):
+        # your code here
+    
+    def vikingAttack(self):
+        # your code here
+
+    def saxonAttack(self):
+        # your code here
+
+    def showStatus(self):
+        # your code here
+
+    pass
+
+'''
